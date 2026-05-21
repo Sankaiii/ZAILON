5963019e6582738edd9def7a9f4c2459ddd0d941
Sankaiii <kai243tv@gmail.com>
2026-05-21 23:10:54 +0200

    fix: remove crash-prone plugins, minimal Rust setup

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,11 +1,6 @@
 use tauri::WebviewWindow;
 
 #[tauri::command]
-fn get_version() -> String {
-    env!("CARGO_PKG_VERSION").to_string()
-}
-
-#[tauri::command]
 fn close_window(window: WebviewWindow) {
     let _ = window.close();
 }
@@ -28,16 +23,13 @@ fn toggle_maximize(window: WebviewWindow) {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_fs::init())
-        .plugin(tauri_plugin_process::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
         .invoke_handler(tauri::generate_handler![
-            get_version,
             close_window,
             minimize_window,
             toggle_maximize
         ])
         .run(tauri::generate_context!())
-        .expect("error while running ZAILON");
+        .unwrap_or_else(|e| {
+            eprintln!("ZAILON error: {}", e);
+        });
 }
